6b93ee3c123615cf8ef8da084bae0c4a45a7f2d0
kharchenkoanastasiia <kharchenko.anastasiia@gmail.com>
2017-12-17 11:43:05 +0200

    first lesson

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    inputValue = '';
+    todosList: string[] = [];
+
+    addToList() {
+        console.log('inputValue', this.inputValue);
+        const dataToSave = this.inputValue.trim();
+
+        if (dataToSave.length === 0) {
+            return;
+        }
+
+        this.todosList.push(dataToSave);
+        this.inputValue = '';
+        console.log('this.todosList', this.todosList);
+    }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {MatCardModule, MatInputModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule} from '@angular/forms';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        MatCardModule,
+        MatInputModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
